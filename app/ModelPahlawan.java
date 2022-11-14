5e8cc3b005eb720d1a5511a68a8af729159ff0f3
DarkLeZx <depniel08@mhs.mdp.ac.id>
2022-11-14 10:59:36 +0700

    Data, Recyler, Card

---
+++ app/ModelPahlawan.java
@@ -0,0 +1,30 @@
+public class ModelPahlawan
+{
+    private String Nama;
+    private String Tentang;
+    private String Foto;
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+
+    public String getTentang() {
+        return Tentang;
+    }
+
+    public void setTentang(String tentang) {
+        Tentang = tentang;
+    }
+
+    public String getFoto() {
+        return Foto;
+    }
+
+    public void setFoto(String foto) {
+        Foto = foto;
+    }
+}
